e3184064cf674810e7b291be38d61008c6970712
abdan10 <abdannur16falah@gmail.com>
2025-06-09 12:03:08 +0700

    puki

--- PHP/restaurant.php
+++ PHP/restaurant.php
@@ -35,5 +35,23 @@ if ($result->num_rows > 0) {
     echo "Data restoran tidak ditemukan.";
 }
 
+// Query insert
+$sql = "INSERT INTO restoran (id_restoran, nama_restoran, lokasi, kategori, jam_operasional)
+        VALUES (1, 'Rumah Makan Padang Raya', 'Jl. Sumagung 3, RT.15/RW.2, Klp. Gading Tim., Kec. Klp. Gading, Jkt Utara', 'Asian Food', '09.00 - 20.00 WIB'),
+               (2, 'Dimsum Sembilan Naga', 'Ruko Paskal Hypersquare Blok G. 2, Jl. Pasir Kaliki No.25 - 27, Ciroyom, Kec. Andir, Kota Bandung, Jawa Barat', 'Asian Food', '06.00 - 21.30 WIB'),
+               (3, 'UNION Plaza Senayan', 'Plaza Senayan, Jl. Asia Afrika No.8 Ground Floor, Gelora, Kecamatan Tanah Abang, Kota Jakarta Pusat,', 'Western Food', '10.00 - 01.00 WIB'),
+               (4, 'Queen's Tandoor', 'Jl. Raya Seminyak No.1, Seminyak, Kec. Kuta, Kabupaten Badung, Bali', 'Indian Food', '12.00 - 00.00 WITA'),
+               (5, 'AL GHIFARI KEBAB & BURGER(G.2)', 'Jl. Bukit Puri Sukamaju, Tanimulya, Kec. Ngamprah, Kabupaten Bandung Barat, Jawa Barat', 'Middle Eastern Food', '09.00 - 17.00 WIB'),
+               (6, 'Opa Resto', 'Jalan Sunter Karya Blok e7 No 14 Kompleks DKI, Sunter Agung Podomoro, RT.3/RW.13, Sunter Agung, Kec. Tj. Priok, North Jakarta', 'Mediterranean Food', '10.00 - 21.00'),
+               (7, 'Nasi Kebuli Mbah Soleh Ngru-Q', 'Jl. Perintis Kemerdekaan No.66, Sondakan, Kec. Laweyan, Kota Surakarta, Jawa Tengah', 'Middle Eastern Food', '08.00- 20.30'),
+               (8, 'DIL RANI RESTAURANT', 'Jl. Komp. Colombo No.50, Mrican, Caturtunggal, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta', 'Indian Food', '10.00 - 22.00'),
+               (9, 'Bottega Ristorante', 'Gedung Fairgrounds No.Kav 52-53, Jl. Scbd No.Lot 14, RT.5/RW.3, Senayan, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta', 'Western Food', '11.00 - 22.00'),
+               (10, 'Blue Olives Taverna', 'Jl. Kawaluyaan II 24-20, Jatisari, Kec. Buahbatu, Kota Bandung, Jawa Barat', 'Mediterranean Food', '07.00 - 22.00')";
+if ($conn->query($sql) === TRUE) {
+    echo "Data restoran berhasil ditambahkan.";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
 $conn->close();
 ?>
